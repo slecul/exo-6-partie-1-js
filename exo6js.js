6550d7acb07d9871c5e9cdbc85d1fd9a667a46ea
slecul <slecul@laposte.net>
2019-01-22 09:27:32 +0100

    v1

---
+++ exo6js.js
@@ -0,0 +1,6 @@
+function valeur() {
+  var nb1 = document.getElementById('firstNumber').value;
+  var nb2 = document.getElementById('secondNumber').value;
+  var result = nb1%nb2;
+  alert(result);
+}
